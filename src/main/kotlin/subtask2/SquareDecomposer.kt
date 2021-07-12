6bb502a00070813b9fe8db71367d0f049019f461
IvanLaptop <sinitsyniv@gmail.com>
2021-07-12 12:55:53 +0300

    feat: SquareDecomposer

--- src/main/kotlin/subtask2/SquareDecomposer.kt
+++ src/main/kotlin/subtask2/SquareDecomposer.kt
@@ -1,9 +1,37 @@
 package subtask2
 
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return decompose(number * number, number - 1)
     }
+
+    private fun decompose(value: Int, start: Int): Array<Int>? {
+        for( i in start downTo 1 ) {
+            val square = i * i
+            val diff = value - square
+            if(diff == 0) {
+                return arrayOf(i)
+            }
+
+            if(diff < 0) {
+                return null
+            }
+
+            var sqrt = sqrt(diff.toDouble()).toInt()
+            if(sqrt >= i) {
+                sqrt = i - 1
+            }
+
+            val res = decompose(diff, sqrt)
+            if( res != null) {
+                return res + arrayOf(i)
+            }
+        }
+        return null
+    }
+
+
 }
